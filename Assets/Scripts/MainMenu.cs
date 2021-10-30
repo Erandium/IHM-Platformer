e305cd5b142e8e88ed08ac66e84defb1c3572d38
Guillaume BRANCHE <guillaume.branche@gmail.com>
2021-10-31 01:05:56 +0200

    first build

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject selectLevel;
+
+    private CanvasGroup mainMenuGroup;
+    private CanvasGroup selectLevelGroup;
+
+    private void Start()
+    {
+        mainMenuGroup = mainMenu.GetComponent<CanvasGroup>();
+        selectLevelGroup = selectLevel.GetComponent<CanvasGroup>();
+    }
     public void QuitGame()
     {
         Application.Quit();
@@ -12,6 +24,24 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("Scene1");
+    }
+
+    public void SelectLevel()
+    {
+        mainMenuGroup.alpha = 0f;
+        mainMenuGroup.blocksRaycasts = false;
+
+        selectLevelGroup.alpha = 1f;
+        selectLevelGroup.blocksRaycasts = true;
+    }
+
+    public void LoadLevel1(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(2);
     }
 
     
